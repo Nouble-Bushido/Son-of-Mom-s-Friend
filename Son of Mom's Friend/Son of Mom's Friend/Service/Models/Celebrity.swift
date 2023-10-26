98fa6e630455b1d1efd5edb8693dd11018c2a7f5
Artem <artchzhen@gmail.com>
2023-10-26 17:29:20 +0900

    added Celebrity, User files

---
+++ Son of Mom's Friend/Son of Mom's Friend/Service/Models/Celebrity.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Son of Mom's Friend
+//
+//  Created by Артем Чжен on 26.10.2023.
+//
+
+import UIKit
+
+struct Celebrity {
+    var name: String
+    var biography: String
+    var dateOfBirth: Date
+    var achievements: [(description: String, date: Date)]
+}
---
+++ Son of Mom's Friend/Son of Mom's Friend/Service/Models/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  Son of Mom's Friend
+//
+//  Created by Артем Чжен on 26.10.2023.
+//
+
+import UIKit
+
+struct User {
+    var dateOfBirth: Date
+}
