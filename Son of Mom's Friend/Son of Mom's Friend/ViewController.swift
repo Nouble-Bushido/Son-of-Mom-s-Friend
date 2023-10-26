1e7cccea99d01afa581ae618e7e4b73795328c97
Artem <artchzhen@gmail.com>
2023-10-26 13:34:37 +0900

    initial commit development

---
+++ Son of Mom's Friend/Son of Mom's Friend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Son of Mom's Friend
+//
+//  Created by Артем Чжен on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
