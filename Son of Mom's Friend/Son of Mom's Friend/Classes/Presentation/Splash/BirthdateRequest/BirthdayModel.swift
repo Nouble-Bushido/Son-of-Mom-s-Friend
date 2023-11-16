627ba92eda0d3dcd5d26bf79bfca1c4e36e333b1
Artem Chzhen <117162015+Nouble-Bushido@users.noreply.github.com>
2023-11-16 14:54:23 +0900

    added BirthdayModel.swift

---
+++ Son of Mom's Friend/Son of Mom's Friend/Classes/Presentation/Splash/BirthdateRequest/BirthdayModel.swift
@@ -0,0 +1,8 @@
+//
+//  BirthdayModel.swift
+//  Son of Mom's Friend
+//
+//  Created by Артем Чжен on 16.11.2023.
+//
+
+import UIKit
---
+++ Son of Mom's Friend/Son of Mom's Friend/Classes/Presentation/Splash/BirthdateRequest/BirthdayView.swift
@@ -0,0 +1,8 @@
+//
+//  BirthdayView.swift
+//  Son of Mom's Friend
+//
+//  Created by Артем Чжен on 16.11.2023.
+//
+
+import UIKit
