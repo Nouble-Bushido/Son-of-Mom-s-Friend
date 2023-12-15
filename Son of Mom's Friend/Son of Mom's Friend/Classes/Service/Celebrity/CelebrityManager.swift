a45f14dacd0e5a34ad3cf16dee87efb7f6be9b46
Artem Chzhen <117162015+Nouble-Bushido@users.noreply.github.com>
2023-12-15 17:42:53 +0900

    refactored CelebrityManager

--- Son of Mom's Friend/Son of Mom's Friend/Classes/Service/Celebrity/CelebrityManager.swift
+++ Son of Mom's Friend/Son of Mom's Friend/Classes/Service/Celebrity/CelebrityManager.swift
@@ -19,36 +19,65 @@ extension CelebrityManager {
         guard
             UserDefaults.standard.data(forKey: Constants.celebrities) == nil,
             let url = Bundle.main.url(forResource: "Celebrity", withExtension: "json"),
-            let data = try? Data(contentsOf: url)
+            let data = try? Data(contentsOf: url),
+            let jsonData = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+            let array = jsonData["celebrities"] as? [[String: Any]]
         else {
             return
         }
-        UserDefaults.standard.set(data, forKey: Constants.celebrities)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        
+        let result = array.compactMap { json -> Celebrity? in
+            guard
+                let id = json["id"] as? Int,
+                let name = json["name"] as? String,
+                let biography = json["biography"] as? String,
+                let photoURL = json["photoURL"] as? String,
+                let dateOfBirthString = json["dateOfBirth"] as? String,
+                let dateOfBirth = dateFormatter.date(from: dateOfBirthString),
+                let achievementIDs = json["achievementIDs"] as? [Int]
+            else {
+                return nil
+            }
+            
+            let celebrity = Celebrity(
+                id: id,
+                name: name,
+                biography: biography,
+                dateOfBirth: dateOfBirth,
+                photoURL: photoURL,
+                achievementIDs: achievementIDs
+            )
+            return celebrity
+        }
+        
+        guard let encoded = try? JSONEncoder().encode(result) else { return }
+        UserDefaults.standard.set(encoded, forKey: Constants.celebrities)
     }
     
     func getCelebrities(birthdate: Date? = nil) -> [Celebrity] {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "yyyy-MM-dd"
-        
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(formatter)
-        
-        guard
-            let data = UserDefaults.standard.data(forKey: Constants.celebrities),
-            let celebrities = try? decoder.decode([Celebrity].self, from: data)
-        else {
-            print("No Data")
-            return []
-        }
-        
-        var filterCelebrities = celebrities
+           guard
+               let data = UserDefaults.standard.data(forKey: Constants.celebrities),
+               let celebrities = try? JSONDecoder().decode([Celebrity].self, from: data)
+           else {
+               return []
+           }
+           var filterCelebrities = celebrities
         
         if let birthday = birthdate {
-            filterCelebrities = filterCelebrities.filter { $0.dateOfBirth == birthday }
-        }
-        return filterCelebrities
-    }
+            let userComponets = Calendar.current.dateComponents([.day, .month], from: birthday)
+            
+              filterCelebrities = filterCelebrities.filter {
+                  
+                  let celebrityComponents = Calendar.current.dateComponents([.day, .month], from: $0.dateOfBirth)
+                  return celebrityComponents.day == userComponets.day && celebrityComponents.month == userComponets.month
+              }
+          }
+           return filterCelebrities
+       }
     
     func getCelebrity(id: Int) -> Celebrity? {
         getCelebrities().first { $0.id == id }
