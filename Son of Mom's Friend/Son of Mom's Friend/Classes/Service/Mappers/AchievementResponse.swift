829a2cdb5b606eea84e041d72c0842048adbbd7a
Artem Chzhen <117162015+Nouble-Bushido@users.noreply.github.com>
2023-12-20 23:29:40 +0900

    added CelebrityResponse, AchievementResponse

--- Son of Mom's Friend/Son of Mom's Friend/Classes/Service/Mappers/AchievementResponse.swift
+++ Son of Mom's Friend/Son of Mom's Friend/Classes/Service/Mappers/AchievementResponse.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+enum AchievementResponse {
+    static func map(from json: [String: Any]) -> [Achievement]? {
+        guard let array = json["achievements"] as? [[String: Any]] else {
+            return nil
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        
+        let result = array.compactMap { json -> Achievement? in
+            guard
+                let id = json["id"] as? Int,
+                let date = json["date"] as? String,
+                let dateAchiev = dateFormatter.date(from: date),
+                let celebrityId = json["celebrityId"] as? Int,
+                let description = json["description"] as? String,
+                let ageAtAchievement = json["ageAtAchievement"] as? Int
+            else {
+                return nil
+            }
+            let achievement = Achievement(
+                id: id,
+                date: dateAchiev,
+                celebrityId: celebrityId,
+                description: description,
+                ageAtAchievement: ageAtAchievement
+            )
+            return achievement
+        }
+        return result
+    }
+}
