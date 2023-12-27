fef8a77cc778f6d90b3d21296a2db49488614238
Artem Chzhen <117162015+Nouble-Bushido@users.noreply.github.com>
2023-12-27 11:32:56 +0900

    added infoView, infoViewController

---
+++ Son of Mom's Friend/Son of Mom's Friend/Classes/Presentation/Info/InfoView.swift
@@ -0,0 +1,12 @@
+//
+//  InfoView.swift
+//  Son of Mom's Friend
+//
+//  Created by Артем Чжен on 27.12.2023.
+//
+
+import UIKit
+
+final class InfoView: UIView {
+    
+}
---
+++ Son of Mom's Friend/Son of Mom's Friend/Classes/Presentation/Info/InfoViewCountroller.swift
@@ -0,0 +1,12 @@
+//
+//  InfoViewCountroller.swift
+//  Son of Mom's Friend
+//
+//  Created by Артем Чжен on 27.12.2023.
+//
+
+import UIKit
+
+final class InfoViewCountroller: UIViewController {
+    
+}
